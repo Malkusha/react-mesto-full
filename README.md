f204cfdbfdbf49ef9ec38b8f8677d8edb804b01e
Pashkova <malkusha@mail.ru>
2023-08-15 19:11:37 +0300

    readme and port 3000

--- README.md
+++ README.md
@@ -5,12 +5,12 @@
 
 
 
-Адрес репозитория: https://github.com/
+Адрес репозитория: https://github.com/Malkusha/react-mesto-full
 
 ## Ссылки на проект
 
-IP-адрес x.x.x.x
+IP-адрес 158.160.14.0
 
-Frontend https://...
+Frontend https://pashkusha.nomoreparties.co
 
-Backend https://...
+Backend https://api.pashkusha.nomoreparties.co
--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/index');
 const { errorHandler } = require('./errors/index');
 
-const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 
 app.use(cors({
