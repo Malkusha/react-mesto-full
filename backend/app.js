257fe867b86814ef2aae4750bae2d3528c7fc15c
Pashkova <malkusha@mail.ru>
2023-08-15 18:58:20 +0300

    crash test

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/index');
 const { errorHandler } = require('./errors/index');
 
-const { PORT = 3001, DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
+const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 
 app.use(cors({
@@ -27,6 +27,12 @@ mongoose.connect(DB_URL);
 
 app.use(helmet());
 app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+}); 
 
 app.use(router);
 
